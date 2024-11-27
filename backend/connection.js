4cc449e02776407258046fb9e66bbb472f74f389
MehdiiCodes <akbarmehdii2627@gmail.com>
2024-11-27 19:18:43 +0530

    MiniProject

---
+++ backend/connection.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const url = "mongodb+srv://mehdiicodes:Akbar27@akbarmehdi.q8tbfar.mongodb.net/socialmediawebapp?retryWrites=true&w=majority&appName=AkbarMehdi";
+
+// asynchroneous function - returns Promise
+mongoose.connect(url)
+    .then((result) => { //thenc, shortcut key.
+        console.log('database conntected');
+    })
+    .catch((err) => {
+    console.log(err);
+    });
+
+module.exports = mongoose;
