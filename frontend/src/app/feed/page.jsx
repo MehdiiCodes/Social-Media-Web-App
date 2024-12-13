a468602e5efacea84021bc0f9dd450d81c2e0aed
MehdiiCodes <akbarmehdii2627@gmail.com>
2024-12-13 15:55:16 +0530

    feed

--- frontend/src/app/feed/page.jsx
+++ frontend/src/app/feed/page.jsx
@@ -55,7 +55,7 @@ const Feed = ({ selCommunity }) => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                 </svg>
               </div>
-              <Link href="/Createpost" className="w-full sm:w-auto">
+              <Link href="/addpost" className="w-full sm:w-auto">
                 <button className="w-full sm:w-auto px-6 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-full transition duration-300 flex items-center justify-center">
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
